ec4ec6b25ff8ab769f2362742549b2c2744e46aa
Shouma Kawamura <ee217088@meiji.ac.jp>
2025-10-11 09:24:12 +0900

    main.dart repair

--- flutter_todo/lib/main.dart
+++ flutter_todo/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// ★ 生成した画面をimport
+// ★ 生成した画面を import
 import 'screens/converted_main.dart';
 import 'screens/add_task_dialog.dart';
 import 'screens/task_item.dart';
@@ -12,19 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Todo',
       theme: ThemeData(primarySwatch: Colors.blue),
 
-      // ★ ここを追加：add-to-app で使うルートを登録
+      // ★ add-to-app 用ルート
       routes: {
         '/converted_main': (_) => const ConvertedMain(),
         '/add_task_dialog': (_) => const AddTaskDialog(),
         '/task_item_demo': (_) => const TaskItem(),
       },
 
-      // 単体起動時は今まで通りカウンター画面を home に
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      // ※ Java から initialRoute を指定して起動した場合は home は無視されます
+      // ★ 単体起動時に表示する画面
+      home: const ConvertedMain(),
     );
   }
 }
